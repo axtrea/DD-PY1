279c8e3d5f7df0d5e9afe59b2bddccff2415eeaa
axtrea <115579402+axtrea@users.noreply.github.com>
2022-10-16 22:05:10 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task9.py
@@ -0,0 +1,12 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+
+need_money = 0  # количество денег, чтобы прожить 10 месяцев
+
+# TODO Оформить решение
+for i in range(0, months):
+    need_money -= salary - spend
+    spend *= (1 + increase)
+print(round(need_money))
